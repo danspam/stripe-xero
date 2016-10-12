334c69019d66a649dc8a42d7a462ec9dacf8d349
Jonah2148 <jsargeant@cnsstu.catholic.edu.au>
2016-10-12 13:55:43 +1000

    Made the date the programe run from automatic, tidied it up abit and after it runs it will now store when it last ran for next time.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ catch (err) {
     console.log("unable to read file 'config.json': ", err);
 }
 
+
 var _ = require("underscore");
 var moment = require("moment");
 var stripe = require("stripe")(config.apiKey);
@@ -13,35 +14,54 @@ var csvWriter = require("csv-write-stream");
 var winston = require('winston');
 var fs = require("fs");
 
-//TODO: CHANGE THIS TO THE CORRECT DATE EACH TIME IT IS RUN
-var since = moment("2015-12-12").unix();
+var lastdatefile = "lastdatefile.dat";
 
-stripe.balance.listTransactions({
-    created: {
-        gt: since
-    },
-    limit: 100
-}).then(function(transactions) {
-    var writer = csvWriter();
-    writer.pipe(fs.createWriteStream('stripestatement_since_'+ since +'.csv'));
-    _.each(transactions.data, function(transaction){
-        writer.write({
-            Date: moment.unix(transaction.created).format("DD/MM/YYYY"),
-            Amount: (transaction.amount/100.0).toFixed(2),
-            Payee: "",
-            Description: transaction.description,
-            Reference: transaction.id
-        });
+fs.readFile(lastdatefile, 'utf8', function (err,data) {
+    
+    if (err) {
+        since = moment("2016-10-1").unix();
+    } else {
+        since = moment(data).unix();
+    }
 
-        if (transaction.fee > 0) {
+    stripe.balance.listTransactions({
+        created: {
+            gt: since
+        },
+        limit: 100
+    }).then(function(transactions) {
+        var writer = csvWriter();
+        writer.pipe(fs.createWriteStream('stripestatement_since_'+ since +'.csv'));
+        _.each(transactions.data, function(transaction){
             writer.write({
                 Date: moment.unix(transaction.created).format("DD/MM/YYYY"),
-                Amount: ((0 - transaction.fee)/100.0).toFixed(2),
-                Payee: "Stripe",
-                Description: "Stripe processing Fee",
+                Amount: (transaction.amount/100.0).toFixed(2),
+                Payee: "",
+                Description: transaction.description,
                 Reference: transaction.id
             });
-        }
-    });
-    writer.end();
-}).catch(winston.error.bind(winston));
+
+            if (transaction.fee > 0) {
+                writer.write({
+                    Date: moment.unix(transaction.created).format("DD/MM/YYYY"),
+                    Amount: ((0 - transaction.fee)/100.0).toFixed(2),
+                    Payee: "Stripe",
+                    Description: "Stripe processing Fee",
+                    Reference: transaction.id
+                });
+            }
+        });
+        writer.end();
+        fs.writeFile(lastdatefile, moment().format() , function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+
+
+    }).catch(winston.error.bind(winston));
+     
+
+});
+
