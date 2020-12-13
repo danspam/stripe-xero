3f0b6e9d5797aab43833d0cb9bce571191798180
Dan Sargeant <dan@keywordintent.com>
2020-12-13 16:56:29 +1000

    Add file logging and upload to dropbox

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 var config;
+var winston = require("winston");
+var Dropbox = require("dropbox").Dropbox;
+require("winston-daily-rotate-file");
+
+//configure logging
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.simple(),
+    transports: [
+      new winston.transports.DailyRotateFile({
+        filename: "stripe-xero.log",
+        dirname: "D:\\node\\stripe-xero",
+        datePattern: "YYYY-MM-DD",
+        prepend: false,
+        level: "info"
+    }),
+    new winston.transports.Console({
+        format: winston.format.simple()
+      })
+    ]
+});
+  
 
 try {
     config = require("./config.json");
-}
-catch (err) {
+} catch (err) {
     config = {};
-    console.log("unable to read file 'config.json': ", err);
+    logger.error(err);
 }
 
 var async = require("async");
 var moment = require("moment");
 var stripe = require("stripe")(config.apiKey);
 var csvWriter = require("csv-write-stream");
-var winston = require("winston");
 var fs = require("fs");
 var since;
 var lastdatefile = "lastdatefile.dat";
@@ -20,8 +40,11 @@ var lastdatefile = "lastdatefile.dat";
 function writeToFile(writer, transaction, charge) {
     writer.write({
         Date: moment.unix(transaction.created).format("DD/MM/YYYY"),
-        Amount: (transaction.amount/100.0).toFixed(2),
-        Payee: charge && charge.customer && charge.customer.description ? charge.customer.description : "",
+        Amount: (transaction.amount / 100.0).toFixed(2),
+        Payee:
+            charge && charge.customer && charge.customer.description
+                ? charge.customer.description
+                : "",
         Description: transaction.description,
         Reference: transaction.source
     });
@@ -29,7 +52,7 @@ function writeToFile(writer, transaction, charge) {
     if (transaction.fee > 0) {
         writer.write({
             Date: moment.unix(transaction.created).format("DD/MM/YYYY"),
-            Amount: ((0 - transaction.fee)/100.0).toFixed(2),
+            Amount: ((0 - transaction.fee) / 100.0).toFixed(2),
             Payee: "Stripe",
             Description: "Stripe processing Fee",
             Reference: transaction.id
@@ -37,51 +60,86 @@ function writeToFile(writer, transaction, charge) {
     }
 }
 
-fs.readFile(lastdatefile, "utf8", function (err,data) {
+function uploadToDropbox(filename) {
+    fs.readFile(filename, "utf8", function (err, data) {
+        if (err) {
+            return logger.error(err);
+        }
+        const dbx = new Dropbox({
+            accessToken:config.dropbox
+          });
 
+          dbx.filesUpload({
+            path: `/stripe-xero/${filename}`,
+            contents: data
+          })
+          .then(() => {
+            logger.info("Upload to dropbox complete");
+          })
+          .catch(err => {
+            logger.error(err);
+          });
+    });
+}
+
+fs.readFile(lastdatefile, "utf8", function (err, data) {
     if (err) {
         since = moment("2016-10-03").unix();
     } else {
         since = moment(data).unix();
     }
 
-    stripe.balanceTransactions.list({
-        created: {
-            gt: since
-        },
-        limit: 300
-    }).then(function(transactions) {
-        var writer = csvWriter();
-        var outFile = "stripestatement_since_"+ since +".csv";
+    stripe.balanceTransactions
+        .list({
+            created: {
+                gt: since
+            },
+            limit: 300
+        })
+        .then(function (transactions) {
+            var writer = csvWriter();
+            var outFile = "stripestatement_since_" + since + ".csv";
 
-        writer.pipe(fs.createWriteStream(outFile));
+            writer.pipe(fs.createWriteStream(outFile));
 
-        async.eachSeries(transactions.data, function(transaction, cb){
-            if (transaction.type === "charge") {
-                stripe.charges.retrieve(transaction.source, {
-                    expand: ["customer"]
-                }, function(err, charge) {
-                    if(err) {
-                        console.log(err);
+            async.eachSeries(
+                transactions.data,
+                function (transaction, cb) {
+                    if (transaction.type === "charge") {
+                        stripe.charges.retrieve(
+                            transaction.source,
+                            {
+                                expand: ["customer"]
+                            },
+                            function (err, charge) {
+                                if (err) {
+                                    logger.error(err);
+                                }
+                                writeToFile(writer, transaction, charge);
+                                cb();
+                            }
+                        );
+                    } else {
+                        writeToFile(writer, transaction, null);
+                        cb();
                     }
-                    writeToFile(writer, transaction, charge);
-                    cb();
-                });
-            } else {
-                writeToFile(writer, transaction, null);
-                cb();
-            }
-        }, function(){
-            writer.end();
+                },
+                function () {
+                    writer.end();
 
-            fs.writeFile(lastdatefile, moment().format() , function(err) {
-                if(err) {
-                    return console.log(err);
+                    fs.writeFile(
+                        lastdatefile,
+                        moment().format(),
+                        function (err) {
+                            if (err) {
+                                return logger.error(err);
+                            }
+                            logger.info("Transactions saved to " + outFile);
+                            uploadToDropbox(outFile);
+                        }
+                    );
                 }
-                console.log("Transactions saved to " + outFile);
-            });          
-        });
-
-    }).catch(winston.error.bind(winston));
+            );
+        })
+        .catch(logger.error.bind(logger));
 });
-
